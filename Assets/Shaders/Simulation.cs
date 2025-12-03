1e3c28f4aaa8446591be0adc0693d5eaf4b7a8d7
LunarianNova <lunariannova@gmail.com>
2025-12-03 14:59:10 -0500

    Spiral Galaxies!

--- Assets/Shaders/Simulation.cs
+++ Assets/Shaders/Simulation.cs
@@ -1,13 +1,5 @@
+using System.Linq;
 using UnityEngine;
-
-public struct OctreeNode // Section 2.1, Hsin Hung
-{
-    Vector4 centerOfMass; // xyz pos, w mass
-    Vector3 boundsMin;
-    Vector3 boundsMax;
-    int firstChildIndex;
-    int particleIndex;
-};
 
 public struct Particle
 {
@@ -18,60 +10,107 @@ public struct Particle
 
 public class Simulation : MonoBehaviour
 {
+    [Header("Simulation Settings")]
     public int particleCount = 1000;
-    public int octreeNodes = 100000;
     public float gravityStrength = 100.0f;
-
     public float maxColorSpeed = 5.0f;
+    public float spawnDiameter = 10f;
 
+    [Header("References")]
     public ComputeShader computeShader;
     public Material particleMaterial;
 
-    private int particleSize = sizeof(float) * 11;
-    private int octreeNodeSize = sizeof(float) * 10 + sizeof(int) * 2;
+    private int particleSize = (16 * 2) + (12 * 1);
     private ComputeBuffer particleBuffer;
-    private ComputeBuffer octreeBuffer;
-    private ComputeBuffer mutexBuffer;
     private int kernelHandle;
-    private int kernelResetTree;
-    private int kernelComputeBoundingBox;
-    private const float THREAD_GROUP_SIZE = 32;
+    private const float THREAD_GROUP_SIZE = 128; // must match compute
 
     void Start()
     {
         kernelHandle = computeShader.FindKernel("CSMain");
-        kernelResetTree = computeShader.FindKernel("CSMain_ResetTree");
-        kernelComputeBoundingBox = computeShader.FindKernel("CSMain_ComputeBoundingBox");
 
         particleBuffer = new ComputeBuffer(particleCount, particleSize);
-        octreeBuffer = new ComputeBuffer(octreeNodes, octreeNodeSize);
-        mutexBuffer = new ComputeBuffer(1, sizeof(int));
-        mutexBuffer.SetData(new int[] { 0 });
 
-        Particle[] particleArray = new Particle[particleCount];
-        for (int i = 0; i < particleCount; i++)
-        {
-            Vector4 positionMass = Random.insideUnitSphere * 10;
-            positionMass.w = 1; // mass
+        Particle[] particleArray = StartSpiral();
 
-            particleArray[i] = new Particle
-            {
-                positionMass = positionMass,
-                velocity = Vector3.zero,
-                color = Vector4.zero,
-            };
-        }
+        //Particle[] particleArray = new Particle[particleCount];
+
+        //for (int i = 0; i < particleCount; i++)
+        //{
+        //    Vector4 pos = Random.insideUnitSphere * 10f;
+        //    pos.w = 1.0f;
+
+        //    particleArray[i] = new Particle
+        //    {
+        //        positionMass = pos,
+        //        velocity = Vector3.zero,
+        //        color = Vector4.zero
+        //    };
+        //}
 
         particleBuffer.SetData(particleArray);
         computeShader.SetBuffer(kernelHandle, "_ParticleBuffer", particleBuffer);
-
-        computeShader.SetBuffer(kernelResetTree, "_OctreeBuffer", octreeBuffer);
-
-        computeShader.SetBuffer(kernelComputeBoundingBox, "_ParticleBuffer", particleBuffer);
-        computeShader.SetBuffer(kernelComputeBoundingBox, "_OctreeBuffer", octreeBuffer);
-        computeShader.SetBuffer(kernelComputeBoundingBox, "_Mutex", mutexBuffer);
-
         particleMaterial.SetBuffer("_ParticleBuffer", particleBuffer);
+    }
+
+    Particle[] StartSpiral(Vector2 centerPos, int particleCount, float diameter)
+    {
+        Particle[] particleArray = new Particle[particleCount];
+        Vector4 posMass;
+
+        float sunMass = 332942f;
+
+        for (int i = 0; i < particleCount - 1; i++)
+        {
+            Vector2 pos = centerPos + Random.insideUnitCircle * diameter;
+            float distance = (pos - centerPos).magnitude;
+
+            Vector2 r = new Vector2(pos.x - centerPos.x, pos.y - centerPos.y);
+            Vector2 a = new Vector2(r.x / distance, r.y / distance);
+            
+            float esc = Mathf.Sqrt((gravityStrength * sunMass) / (distance));
+            Vector2 velocity = new Vector2(-a.y * esc, a.x * esc);
+            //Debug.Log(pos);
+            //Debug.Log(velocity);
+
+            posMass = pos;
+            posMass.w = 1;
+            particleArray[i] = new Particle
+            {
+                positionMass = posMass,
+                velocity = velocity,
+                color = Vector4.zero
+            };
+        }
+
+        posMass = centerPos;
+        posMass.w = sunMass;
+        particleArray[particleCount - 1] = new Particle
+        {
+            positionMass = posMass,
+            velocity = Vector3.zero,
+            color = Vector4.zero
+        };
+
+        return particleArray;
+    }
+
+    Particle[] StartSpiral()
+    {
+        return StartSpiral(Vector2.zero, particleCount, spawnDiameter);
+    }
+
+    Particle[] StartCollidingSpirals(float distance)
+    {
+        Particle[] galaxyOne = StartSpiral(new Vector2(distance, 0), particleCount/2, spawnDiameter/2);
+        Particle[] galaxyTwo = StartSpiral(new Vector2(-distance, 0), particleCount/2, spawnDiameter/2);
+
+        return galaxyOne.Concat(galaxyTwo).ToArray();
+    }
+
+    Particle[] StartCollidingSpirals()
+    {
+        return StartCollidingSpirals(spawnDiameter);
     }
 
     void Update()
@@ -81,15 +120,8 @@ public class Simulation : MonoBehaviour
         computeShader.SetFloat("_GravityStrength", gravityStrength);
         computeShader.SetFloat("_MaxColorSpeed", maxColorSpeed);
         computeShader.SetFloat("_ParticleCount", particleCount);
-        computeShader.SetInt("_OctreeNodeCount", octreeNodes);
-        computeShader.SetInt("_RootNodeIndex", 0);
-        mutexBuffer.SetData(new int[] { 0 });
 
-        int octreeThreadGroups = Mathf.CeilToInt(octreeNodes / THREAD_GROUP_SIZE);
         int particleThreadGroups = Mathf.CeilToInt(particleCount / THREAD_GROUP_SIZE);
-
-        //computeShader.Dispatch(kernelResetTree, octreeThreadGroups, 1, 1);
-        //computeShader.Dispatch(kernelComputeBoundingBox, particleThreadGroups, 1, 1);
         computeShader.Dispatch(kernelHandle, particleThreadGroups, 1, 1);
     }
 
@@ -104,14 +136,6 @@ public class Simulation : MonoBehaviour
         if (particleBuffer != null)
         {
             particleBuffer.Release();
-        }
-        if (octreeBuffer != null)
-        {
-            octreeBuffer.Release();
-        }
-        if (mutexBuffer != null)
-        {
-            mutexBuffer.Release();
         }
     }
 }
